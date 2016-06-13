f95ea73b6aa0bb5eadb4fbdf02d33fbc7846555b
mirhosting <dev@mirhosting.com>
2016-06-13 17:39:36 +0200

    Update install_wrapper.sh

--- install_wrapper.sh
+++ install_wrapper.sh
@@ -3,4 +3,5 @@
 
 /usr/bin/wget http://download.ispsystem.com/install.sh -O /usr/local/src/install.sh
 /usr/bin/chmod +x /usr/local/src/install.sh
+/usr/bin/hostname build.docker.com
 /bin/sh -c "/usr/local/src/install.sh --osfamily REDHAT --osversion 7 --release stable --disable-fail2ban --silent ISPmanager-Lite"
